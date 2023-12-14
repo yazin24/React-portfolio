a01fd8dc39d852734b67b7889de3ead12a5da15c
yazin24 <yazinconiendo@gmail.com>
2023-12-14 20:08:03 +0800

    ....

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -5,8 +5,10 @@ import Fave1 from './fave1.png';
 import Fave2 from './fave2.png';
 import Shappy from './shappy.png';
 import Shappy1 from './shappy1.png';
+import Shappy2 from './shappy2.png';
 import Tracker from './tracker.png';
 import Tracker1 from './tracker1.png';
+import Tracker2 from './tracker2.png';
 
 const Projects = () => {
   return (
@@ -22,19 +24,19 @@ const Projects = () => {
 
         <div className="">
 
-          <div className=" bg-white flex flex-col lg:flex-row">
+          <div className=" bg-white flex flex-col lg:flex-row mt-8">
 
             <div className="p-2 flex flex-col lg:flex-row">
               <div className="flex flex-col lg:flex-row w-full">
                 <img src={Fave2} className="w-full lg:hidden" />
                 <img src={Fave} className="hidden lg:block lg:w-1/4" />
                 <img src={Fave1} className="hidden lg:block lg:w-1/4" />
-                <div className="lg:mx-8">
+                <div className="lg:mx-8 my-8">
                   <h2 className="text-xl lg:text-4xl font-bold text-cyan-400">FAVE ECOMMERCE INC.</h2>
-                  <p className="md:text-2xl my-8">Make a web based system using the LAMPSTACK(LINUX, APACHE, MYSQL, PHP).</p>
+                  <p className="md:text-xl lg:text-2xl my-8">Make a web based system using the LAMPSTACK(LINUX, APACHE, MYSQL, PHP).</p>
                   <h2 className="font-bold mb-2">Stacks Use: </h2>
 
-                  <div className="flex flex-col md:flex-row lg:gap-8">
+                  <div className="flex flex-col md:flex-row md:gap-8">
                   <div className="flex flex-col gap-2">
                   <h3 className="text-cyan-400 font-bold">TAILWIND</h3>
                   <h3 className="text-cyan-400 font-bold">CSS</h3>
@@ -49,37 +51,78 @@ const Projects = () => {
                   <h3 className="text-cyan-400 font-bold">LINUX</h3>
                   </div>
                   </div>
-                   
+                   <button className="bg-cyan-400 hover:bg-cyan-500 hover:shadow-md p-4 font-bold text-white my-8"><a href="https://github.com/yazin24/fave_system">GITHUB LINK HERE</a></button>
               </div>
               </div>
               
             </div>
           </div>
 
-          <div className="lg:m-24 bg-white flex flex-col lg:flex-row">
+          {/* shappy */}
 
-            <div>
-              dfdfd
+          <div className=" bg-white flex flex-col lg:flex-row mt-8">
+
+            <div className="p-2 flex flex-col lg:flex-row">
+              <div className="flex flex-col lg:flex-row w-full">
+                <img src={Shappy2} className="w-full lg:hidden" />
+                
+                <div className="lg:mx-8 my-8">
+                  <h2 className="text-xl lg:text-4xl font-bold text-cyan-400">SHAPPY CAPSTONE PROJECT</h2>
+                  <p className="md:text-xl lg:text-2xl my-8">As part of a 3 man team, create a fully functional ecommerce system using PHP and LARAVEL with MYSQl as database. </p>
+                  <h2 className="font-bold mb-2">Stacks Use: </h2>
+
+                  <div className="flex flex-col md:flex-row md:gap-8">
+                  <div className="flex flex-col gap-2">
+                  <h3 className="text-cyan-400 font-bold">TAILWIND</h3>
+                  <h3 className="text-cyan-400 font-bold">CSS</h3>
+                  <h3 className="text-cyan-400 font-bold">JAVASCRIPT</h3>
+                  <h3 className="text-cyan-400 font-bold">PHP</h3>
+                  </div>
+                  
+                  <div className="flex flex-col gap-2">
+                  <h3 className="text-cyan-400 font-bold">LARAVEL</h3>
+                  <h3 className="text-cyan-400 font-bold">MYSQL</h3>
+                  </div>
+                  </div>
+                   <button className="bg-cyan-400 hover:bg-cyan-500 hover:shadow-md p-4 font-bold text-white my-8"><a href="https://github.com/yazin24/capstoneproject">GITHUB LINK HERE</a></button>
+              </div>
+              <img src={Shappy} className="hidden lg:block lg:w-1/4" />
+                <img src={Shappy1} className="hidden lg:block lg:w-1/4" />
+              </div>
             </div>
-
-            <div className="p-8 flex flex-row justify-end">
-              <img src={Shappy} className="w-1/4" />
-              <img src={Shappy1} className="w-1/4" />
-            </div>
-
-
           </div>
-          <div className="lg:m-24 bg-white flex flex-col lg:flex-row">
 
-            <div className="p-8 flex flex-row">
-              <img src={Tracker} className="w-1/4" />
-              <img src={Tracker1} className="w-1/4" />
+          {/* covid tracker */}
+
+          <div className=" bg-white flex flex-col lg:flex-row mt-8">
+
+            <div className="p-2 flex flex-col lg:flex-row">
+              <div className="flex flex-col lg:flex-row w-full">
+                <img src={Tracker2} className="w-full lg:hidden" />
+                <img src={Tracker} className="hidden lg:block lg:w-1/4" />
+                <img src={Tracker1} className="hidden lg:block lg:w-1/4" />
+                <div className="lg:mx-8 my-8">
+                  <h2 className="text-xl lg:text-4xl font-bold text-cyan-400">COVID-19 TRACKER</h2>
+                  <p className="md:text-xl lg:text-2xl my-8">A personal project created using react js and covid-19 api.</p>
+                  <h2 className="font-bold mb-2">Stacks Use: </h2>
+
+                  <div className="flex flex-col md:flex-row md:gap-8">
+                  <div className="flex flex-col gap-2">
+                  <h3 className="text-cyan-400 font-bold">TAILWIND</h3>
+                  <h3 className="text-cyan-400 font-bold">CSS</h3>
+                  <h3 className="text-cyan-400 font-bold">JAVASCRIPT</h3>
+                  </div>
+                  
+                  <div className="flex flex-col gap-2">
+                  <h3 className="text-cyan-400 font-bold">REACT JS</h3>
+                  <h3 className="text-cyan-400 font-bold">COVID-19 API</h3>
+                  </div>
+                  </div>
+                   <button className="bg-cyan-400 hover:bg-cyan-500 hover:shadow-md p-4 font-bold text-white my-8"><a href="https://github.com/yazin24/Covid19-tracker">GITHUB LINK HERE</a></button>
+              </div>
+              </div>
+              
             </div>
-
-            <div>
-
-            </div>
-
           </div>
 
         </div>
