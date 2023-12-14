62fbb187e38581becb242c317903b8ea5f26c2f4
yazin24 <yazinconiendo@gmail.com>
2023-12-14 16:27:34 +0800

    .....

--- src/pages/Aboutme.jsx
+++ src/pages/Aboutme.jsx
@@ -6,67 +6,52 @@ import { motion } from "framer-motion";
 const Aboutme = () => {
   return (
     <motion.div
-      className="about-me text-white text-sm m-8"
+      className="text-sm m-8"
       initial={{ opacity: 0 }}
       animate={{ opacity: 2 }}
       exit={{ opacity: 0 }}
     >
-      <div>
-        <h2 className="text-center text-black text-xl mb-4">ABOUT ME</h2>
-      <div className="flex flex-col md:flex-row md:gap-8 p-8">
+      <h2 className="text-center font-bold text-gray-900">ABOUT ME</h2>
+
+      <div className="flex flex-col lg:flex-row">
+
+      <div className="flex flex-col gap-8 mt-4 lg:mt-24 lg:mx-12">
+
+        <div className="flex flex-col lg:flex-row gap-8">
+
+          <div className="bg-white text-cyan-500 py-16 px-30 lg:px-32 text-center">SAMPLE</div>
+          <div className="bg-white text-cyan-500 py-16 px-30 lg:px-32 text-center">SAMPLE</div>
+
+        </div>
+
+        <div className="flex flex-col lg:flex-row gap-8">
+
+          <div className="bg-white text-cyan-500 py-16 px-30 lg:px-32 text-center">SAMPLE</div>
+          <div className="bg-white text-cyan-500 py-16 px-30 lg:px-32 text-center">SAMPLE</div>
+
+        </div>
+
+      </div>
+
+      <div className="mt-16 lg:mt-24 pb-16">
+        <h1 className="text-xl font-bold">A FULLSTACK DEVELOPER BASED IN PHILIPPINES</h1>
+        <p className="my-8 lg:text-xl">Navigating the intricate maze of code, I uncover the artistry in logic,<br></br> where every keystroke is a brushstroke painting the canvas of a virtual masterpiece.</p>
         
-      <div className="my-image flex justify-center items-center align-center md:z-50">
-        <img src={Iris}/>
+        <div className="flex flex-col lg:flex-row lg:gap-8">
+        <div>
+        <h2 className="mb-8 font-bold">Name: <span className="text-cyan-400 font-bold ml-4"> JOHN IRIS CONIENDO</span></h2>
+        <h2 className="mb-8 font-bold">Birthdate: <span className="text-cyan-400 font-bold ml-4"> DECEMBER 14, 1996</span></h2>
+        <h2 className="mb-8 font-bold">Address: <span className="text-cyan-400 font-bold ml-4"> SAN JUAN CAINTA RIZAL</span></h2>
+        </div>
+       <div>
+       <h2 className="mb-8 font-bold">Zip code: <span className="text-cyan-400 font-bold ml-4">1900</span></h2>
+        <h2 className="mb-8 font-bold">Email: <span className="text-cyan-400 font-bold ml-4">YAZINCONIENDO@GMAIL.COM</span></h2>
+        <h2 className="mb-8 font-bold">Phone: <span className="text-cyan-400 font-bold ml-4">09994685156 / 09352703145</span></h2>
+       </div>
+        </div>
+        <button className="bg-cyan-400 hover:bg-cyan-500 p-2 font-bold text-white hover:shadow-md mt-4">DOWNLOAD CV</button>
       </div>
 
-      <div className="myself flex flex-col justify-center">
-        <h1 className="title text-black text-xl md:text-2xl  pb-2">
-          John Iris Coniendo
-        </h1>
-        <br />
-        <p className="text-xs md-text-md text-black md:text-lg pb-2">
-          A dedicated Web Developer with a proven ability to adapt in both
-          self-starting and collaborative environments while staying focused on
-          achieving high quality results under strict deadlines. Eager to obtain
-          a challenging position at any prestigious company that will expand my
-          learning and build upon my developer skills.
-        </p>
-
-      
-        <h2 className="title text-black text-xl md:text-2xl pb-2 mb-2">Skills:</h2>
-        <p className="text-black text-xs md:text-md pb-2">
-          HTML | CSS | BOOTSTRAP | TAILWIND | JAVASCRIPT | PHP | LARAVEL | MYSQL | SQL |
-          REACT | REACT-NATIVE | NODE JS | EXPRESS JS | MONGODB
-        </p>
-      
-
-        <ul className="flex gap-4 text-green-700">
-          <li>
-            <a
-              href="https://www.facebook.com/johniris.coniendo"
-              className="text-lg md:text-2xl"
-            >
-              <FaFacebookSquare />
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://www.linkedin.com/in/john-iris-coniendo-08953b225/"
-              className="text-lg md:text-2xl"
-            >
-              <FaLinkedin />
-            </a>
-          </li>
-          <li>
-            <a href="https://github.com/yazin24" className="text-lg md:text-2xl">
-              <FaGithubSquare />
-            </a>
-          </li>
-        </ul>
-
-      
-      </div>
-      </div>
       </div>
     </motion.div>
   );
