8bd0c03089de803e9ca1932c0433e7d96c20b7c4
yazin24 <yazinconiendo@gmail.com>
2023-12-14 18:43:03 +0800

    .....

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,8 +1,9 @@
 import CovidTracker from "./covidtracker.mp4";
-import Shappy from "./shappy1.mp4";
 import Ennea from "./ennea.mp4";
-import Fave from "./fave_system.mp4";
 import { motion } from "framer-motion";
+import Fave from './fave.png';
+import Shappy from './shappy.png';
+import Tracker from './tracker.png';
 
 const Projects = () => {
   return (
@@ -17,9 +18,9 @@ const Projects = () => {
        <h2 className="text-center font-bold">WORK EXPERIENCES AND PROJECTS</h2> 
 
        <div>
-        <div className="bg-white">
-          <div>
-            <img/>
+        <div className="bg-white flex flex-col lg:flex-row">
+          <div className="p-8">
+            <img src={Fave} className="w-1/4"/>
           </div>
 
           <div>
