9d2d8f50acf7b018f422b2e41c8f34b0c497942f
yazin24 <yazinconiendo@gmail.com>
2023-12-15 19:55:11 +0800

    .....

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -33,7 +33,7 @@ const Contact = () => {
 
         <h2 className="text-center font-bold text-xl my-8">CONTACT ME</h2>
 
-      <div className="flex flex-col justify-center mx-auto md:flex-row bg-white text-cyan-500 font-bold md:w-3/4 m-4">
+      <div className="flex flex-col justify-center mx-auto md:flex-row bg-white text-cyan-500 font-bold md:w-3/4 m-4 mb-24">
 
         <div className="md:w-1/2 flex flex-col justify-center text-xl gap-4 bg-cyan-400 text-white font-bold py-4">
           <h2 className="text-center">Address: Oriole st. San Juan Cainta, Rizal</h2>
@@ -46,13 +46,13 @@ const Contact = () => {
           </div>
         </div>
 
-        <div className="p-8 md:w-1/2 mb-4">
+        <div className="p-8 md:w-1/2">
           <form onSubmit={submitSuccess}>
-            <input type="text" placeholder="Name" className="text-xl md:text-2xl border border-2 rounded-sm w-full mb-2" required/>
-            <input type="text" placeholder="Email" className="text-xl md:text-2xl border border-2 rounded-sm w-full mb-2" required/>
-            <input type="text" placeholder="Subject" className="text-xl md:text-2xl border border-2 rounded-sm w-full mb-4" required/>
+            <input type="text" placeholder="Name" className="text-xl md:text-2xl p-1 border border-2 rounded-sm w-full mb-2" required/>
+            <input type="text" placeholder="Email" className="text-xl md:text-2xl p-1 border border-2 rounded-sm w-full mb-2" required/>
+            <input type="text" placeholder="Subject" className="text-xl md:text-2xl p-1 border border-2 rounded-sm w-full mb-4" required/>
             <label className="text-xl md:text-2xl">Message:</label>
-            <textarea name="message" id="" cols="20" rows="7" className="text-xl border border-2 rounded-sm w-full" required></textarea>
+            <textarea name="message" id="" cols="20" rows="7" className="text-xl p-1 border border-2 rounded-sm w-full" required></textarea>
             <button type="submit" className="bg-cyan-400 hover:bg-cyan-500 text-white hover:shadow-md px-2 py-1 font-bold">Submit</button>
           </form>
           <SubmitSuccessMessage show={showMessage} handleCloseMessage={handleCloseMessage}/>
